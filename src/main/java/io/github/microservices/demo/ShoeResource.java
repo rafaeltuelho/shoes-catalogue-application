d4a0e2b5acc60c8e83854ff673f7331014628ddf
Rafael T. C. Soares <rafaelcba@gmail.com>
2019-05-12 17:14:48 -0300

    Http response exception handler and new images

--- src/main/java/io/github/microservices/demo/ShoeResource.java
+++ src/main/java/io/github/microservices/demo/ShoeResource.java
@@ -3,12 +3,16 @@ package io.github.microservices.demo;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
@@ -41,4 +45,21 @@ public class ShoeResource {
     public String hello() {
         return "hello";
     }
+
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+
+        @Override
+        public Response toResponse(Exception exception) {
+            int code = 500;
+            if (exception instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+            return Response.status(code)
+                    .entity(Json.createObjectBuilder().add("error", exception.getMessage()).add("code", code).build())
+                    .build();
+        }
+
+    }
+
 }
