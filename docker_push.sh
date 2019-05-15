ddf46e18f65ca17c9e0f9a55dec52b24245e8991
Rafael T. C. Soares <rafaelcba@gmail.com>
2019-05-14 21:43:43 -0300

    using stdin for docker password

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$REGISTRY_AUTH_PASSWORD" | docker login -u "$REGISTRY_AUTH_USER" --password-stdin
+echo "$REGISTRY_AUTH_PASSWORD" | docker login -u "$REGISTRY_AUTH_USER" --password-stdin $REGISTRY_SERVER
 docker push ${REGISTRY_SERVER}/${TRAVIS_REPO_SLUG}
